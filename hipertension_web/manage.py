4b8b5f9413e68fa1861c4c05eb52cbe33bccd8cc
Jesus Lopez <2223050507@untels.edu.pe>
2025-05-12 23:37:21 -0500

    Revision del Excel, API y Django - Tomar atributos importantes

--- hipertension_web/manage.py
+++ hipertension_web/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "breast_cancer_web.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hipertension_web.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
