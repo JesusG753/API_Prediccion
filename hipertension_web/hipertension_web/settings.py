1bb45f0f1687fce6ff4c11ed318b16a3426712c2
Jesus Lopez <2223050507@untels.edu.pe>
2025-05-19 21:52:22 -0500

    Formulario actualizado con las observaciones dadas

--- hipertension_web/hipertension_web/settings.py
+++ hipertension_web/hipertension_web/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'predictor',  # añadido
+    'predictor',  
 ]
 
 MIDDLEWARE = [
@@ -115,9 +115,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# === Configuración de correo SMTP ===
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'jesus.juegos.08@gmail.com'         
+EMAIL_HOST_PASSWORD = 'qfvl aoko exzi zibn' 
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
--- hipertension_web/predictor/urls.py
+++ hipertension_web/predictor/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.predict, name='predict'),
+    path('send-report/', views.send_report, name='send_report'),
+    path('clear-session/', views.clear_session, name='clear_session'),
 ]
